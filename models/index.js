fb95b3cf0e336f3257773e1d715bc519f4e1f20e
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-09 19:20:36 +0100

    index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
-const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
