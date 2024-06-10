01e086ecc3a076f3a7d39bfeda2c2e26532fd717
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-06-10 17:21:49 +0100

    index js and product routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
-const Product = require('./Product');
 // The `/api/products` endpoint
 
 // get all products
